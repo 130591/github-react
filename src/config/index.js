848492a99338a9b293f7a875b35bfbfb5707dafd
everton paixao <everton.paixao16@gmail.com>
2019-08-18 22:26:11 -0300

    create full config app

---
+++ src/config/index.js
@@ -0,0 +1,5 @@
+
+
+export const CLIENT_ID = '303bd364aa2d4554ff9a'
+export const REDIRECT_URI = 'https://localhost/login/callback';
+export const  CLIENT_SECRET = 'aaae164d918934c02bb1aeb7fce979f8f306b7b6';
