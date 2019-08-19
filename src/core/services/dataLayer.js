22aaae4a1bd7470bc466f850584200deb4012f84
everton paixao <everton.paixao16@gmail.com>
2019-08-18 22:25:35 -0300

    change datalayer of object for a class

--- src/core/services/dataLayer.js
+++ src/core/services/dataLayer.js
@@ -1,11 +1,57 @@
 import api from "./index";
 
 class DataLayer {
-  static async read(url) {
-    const resp = await api.get(url);
+  static async overview() {
+    const resp = await api.get("");
+
+    return resp;
+  }
+
+  static async repositories() {
+    const resp = await api.get("");
+
+    return resp;
+  }
+
+  static async stars() {
+    const resp = await api.get();
+
+    return resp;
+  }
+
+  static async projects() {
+    const resp = await api.get("");
+
+    return resp;
+  }
+
+  static async followers() {
+    const resp = await api.get("");
+
+    return resp;
+  }
+
+  static async following() {
+    const resp = await api.get("");
 
     return resp;
   }
 }
 
 export default DataLayer;
+
+export class DataRepos {
+  static async chooseTabs(tab) {
+    if (tab === "overview") DataLayer.overview();
+
+    if (tab === "repositores") DataLayer.repositories();
+
+    if (tab === "stars") DataLayer.stars();
+
+    if (tab === "projects") DataLayer.projects();
+
+    if (tab === "followers") DataLayer.followers();
+
+    if (tab === "following") DataLayer.following();
+  }
+}
