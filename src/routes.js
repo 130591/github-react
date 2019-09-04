dd80a3b92ff4beca7efc57c01a9ab90601e2566b
everton paixao <everton.paixao16@gmail.com>
2019-09-04 14:00:07 -0300

    creating routes

--- src/routes.js
+++ src/routes.js
@@ -1,16 +1,28 @@
 import React from "react";
-import { Route, Switch } from "react-router";
+import { Route, Switch, Redirect } from "react-router";
 import { BrowserRouter } from "react-router-dom";
 
 // PAGES COMPONENTS
 import PageProfile from "./views/pages/profile";
+import Login from "./views/pages/login";
 
-export function Routes() {
+export function Routes(props) {
   return (
     <BrowserRouter>
       <Switch>
-        <Route exact path="/" component={PageProfile} />
+        <Route exact path="/login" component={Login} />
+        <Route
+          exact
+          path="/"
+          render={props =>
+            !!props.user ? (
+              <Redirect to="/login" />
+            ) : (
+              <Route exact path="/" component={PageProfile} />
+            )
+          }
+        />
       </Switch>
     </BrowserRouter>
   );
-};
+}
