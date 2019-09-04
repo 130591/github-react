5fc1b574a03ff78c7ecfae4a418a9b162064a36a
everton paixao <everton.paixao16@gmail.com>
2019-09-04 14:05:34 -0300

    card define

--- src/views/components/UI/card/index.js
+++ src/views/components/UI/card/index.js
@@ -1,24 +1,24 @@
 import React from "react";
+import { Link } from "react-router-dom";
 
 // COMPONENTS
 import { DefaultButton } from "../buttons/style";
 import { Box } from "./style";
 import { CardContent, CardBody, CardInfo, Avatar, Details } from "./style";
 
-export const CardProfile = () => {
+export const CardProfile = ({ avatar, name, bio, location, login }) => {
   return (
     <CardContent>
-      <Avatar
-        src={"https://avatars0.githubusercontent.com/u/20320981?s=400&v=4"}
-        alt="avatar"
-      />
+      {avatar && <Avatar src={avatar} alt="avatar" />}
       <CardBody>
-        <h2>Paul Fitzgerald</h2>
-        <h3>Pau1fitz</h3>
+        {name && <h2>{name}</h2>}
+        {login && <h3>{login}</h3>}
         <DefaultButton>follow</DefaultButton>
         <CardInfo>
-          <Details>rio de janeiro</Details>
-          <Details>everton.paixao16@gmail.com</Details>
+          {location && <Details>{location}</Details>}
+          <Details>
+            <Link to="">everton.paixao16@gmail.com</Link>
+          </Details>
         </CardInfo>
       </CardBody>
     </CardContent>
