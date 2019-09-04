779625b846b4d4f8daf045aba78bb16236c631e8
everton paixao <everton.paixao16@gmail.com>
2019-09-04 14:08:07 -0300

    updating container profile

--- src/views/components/containers/profile/index.js
+++ src/views/components/containers/profile/index.js
@@ -3,11 +3,23 @@ import React from "react";
 // COMPONENTS
 import { Content } from "./style";
 import { CardProfile } from "../../UI/card/";
+//  CONTEXT
+import { ProfileData } from "../../../pages/profile/context";
 
 const Profile = () => {
   return (
     <Content>
-      <CardProfile />
+      <ProfileData.Consumer>
+        {overview => (
+          <CardProfile
+            name={overview.name}
+            avatar={overview.avatar_url}
+            bio={overview.bio}
+            location={overview.location}
+            login={overview.login}
+          />
+        )}
+      </ProfileData.Consumer>
     </Content>
   );
 };
