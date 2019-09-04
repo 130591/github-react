50a28a5d6f181ce5127953142b46bddcc55d9b5f
everton paixao <everton.paixao16@gmail.com>
2019-09-04 13:57:48 -0300

    updating reference of inicial scream

--- src/App.js
+++ src/App.js
@@ -1,7 +1,7 @@
-import React from "react";
+import React, { useState } from "react";
 
 import GlobalStyles from "./config/globalStyles";
-import  { Routes } from "./routes";
+import { Routes } from "./routes";
 
 function App() {
   return (
